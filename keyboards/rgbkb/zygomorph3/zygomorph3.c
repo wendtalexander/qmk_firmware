e0b9be82b6cf2e056f02ae8b8e927277b24844aa
Legonut <legonut3@gmail.com>
2022-11-03 17:25:19 -0400

    Change file names

--- keyboards/rgbkb/zygomorph3/zygomorph3.c
+++ keyboards/rgbkb/zygomorph3/zygomorph3.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "zygo3.h"
+#include "zygomorph3.h"
 
 #ifdef OLED_ENABLE
 
