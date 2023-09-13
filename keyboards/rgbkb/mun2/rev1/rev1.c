5dd5d64e477bcd071ea57d10304589293bf19647
Legonut <legonut3@gmail.com>
2023-09-13 17:57:39 -0400

    Fix Mun2 LEDs

--- keyboards/rgbkb/mun2/rev1/rev1.c
+++ keyboards/rgbkb/mun2/rev1/rev1.c
@@ -80,23 +80,23 @@ bool touch_encoder_tapped_kb(uint8_t index, uint8_t section) {
 #ifdef RGB_MATRIX_ENABLE
 // clang-format off
 led_config_t g_led_config = { {
-    {   0,   1,   2,   3,   4,   5,   6},
-    {  16,  15,  13,  11,  10,   8,   7},
-    {  17,  18,  19,  20,  21,  22,  23},
-    {  30,  29,  28,  27,  26,  25,  24},
-    {  31,  32,  33,  34,  35,  36,  37},
+    {   0, NO_LED,   1,   2,   3,   4,   5,   6},
+    {  NO_LED, 13,  12,  11,  10,   9,   8,   7},
+    {  NO_LED, 14,  15,  16,  17,  18,  19,  20},
+    {  NO_LED, 27,  26,  25,  24,  23,  22,  21},
+    {  28, NO_LED,  29,  30,  31,  32,  33,  34},
     
-    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
+    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
+    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
 
-    {  50,  51,  52,  53,  54,  55,  56},
-    {  63,  62,  61,  60,  59,  58,  57},
-    {  64,  65,  66,  68,  69,  70,  72},
-    {  79,  78,  77,  76,  75,  74,  73},
-    {  80,  81,  82,  83,  84,  85,  86},
+    {  35, NO_LED,  36,  37,  38,  39,  40,  41},
+    {  NO_LED, 48,  47,  46,  45,  44,  43,  42},
+    {  NO_LED, 49,  50,  51,  52,  53,  54,  55},
+    {  NO_LED, 62,  61,  60,  59,  58,  57,  56},
+    {  63, NO_LED,  64,  65,  66,  67,  68,  69},
     
-    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
-    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED }
+    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED },
+    {  NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED, NO_LED }
 }, { 
     {  11,  16},{  34,  16},{  49,  16},{  64,  16},{  79,  16},{  94,  16},{ 109,  16},
     { 109,  31},{  94,  31},{  79,  31},{  64,  31},{  49,  31},{  34,  31},{  11,  31},
